93a63c2625bf84b584c98b67b1c7d9eb6f5c993f
Chris Burton <chris@fullstackcaveman.com>
2021-10-15 11:27:29 -0600

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import Home from './components/Home';
-import { useGA4React } from 'ga-4-react';
 
 const App = () => {
-	const ga = useGA4React();
-	console.log(ga);
-
 	return (
 		<>
 			<Home />;
