822b830357c2c3e73a38a7cd12c702e5949601b7
Chris Burton <chris@fullstackcaveman.com>
2021-10-15 11:25:06 -0600

    seo google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,17 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/main.css';
+import GA4React from 'ga-4-react';
 
-ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
-	document.getElementById('root')
-);
+const ga4react = new GA4React('G-PS3ZG01431');
+
+(async () => {
+	await ga4react.initialize();
+
+	ReactDOM.render(
+		<Router>
+			<App />
+		</Router>,
+		document.getElementById('root')
+	);
+})();
