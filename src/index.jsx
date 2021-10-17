76c6e22ca09761510415b151a84f4cc41beac6a8
Chris Burton <chris@fullstackcaveman.com>
2021-10-16 21:06:02 -0600

    removes gtm

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/main.css';
 import GA4React from 'ga-4-react';
-import TagManager from 'react-gtm-module';
 
 const ga4react = new GA4React('G-PS3ZG01431');
 
 (async () => {
 	await ga4react.initialize();
-	await TagManager.initialize({ gtmId: 'GTM-T3M6LDP' });
 
 	ReactDOM.render(
 		<Router>
